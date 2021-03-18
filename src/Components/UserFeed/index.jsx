052d4ee9e0fe103401356027101aceb7504ec4ed
unknown <tuanbinh.35@gmail.com>
2021-03-18 17:44:52 +0200

    finish user detail feed layout

--- src/Components/UserFeed/index.jsx
+++ src/Components/UserFeed/index.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
 import MarkunreadMailboxIcon from "@material-ui/icons/MarkunreadMailbox";
 import ImageIcon from "@material-ui/icons/Image";
-import { Nav, Tab, TabContainer } from "react-bootstrap";
+import { Nav, Row, Tab, TabContainer } from "react-bootstrap";
 import UserPost from "../../Components/UserPost/index";
 import { useStateValue } from "../../Context API/StateProvider";
 import firebase from "firebase";
@@ -55,14 +55,61 @@ const UserFeed = () => {
             <Box className="userFeed__body__bottom">
               <Tab.Content>
                 <Tab.Pane eventKey="first">
-                  <UserPost
-                    profilePic={user.photoURL}
-                    displayName={user.displayName}
-                    likeCount="12"
-                    commentCount="15"
-                    postImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEQ8h23j8rVIwS2NlznjR9ZOhhk3L9OGzWg&usqp=CAU"
-                    user={user}
-                  />
+                  <Row>
+                    <UserPost
+                      profilePic={user.photoURL}
+                      displayName={user.displayName}
+                      likeCount="12"
+                      commentCount="15"
+                      postImage="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEQ8h23j8rVIwS2NlznjR9ZOhhk3L9OGzWg&usqp=CAU"
+                      user={user}
+                    />
+
+                    <UserPost
+                      profilePic={user.photoURL}
+                      displayName={user.displayName}
+                      likeCount="12"
+                      commentCount="15"
+                      postImage="https://images.unsplash.com/photo-1601758174114-e711c0cbaa69?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                      user={user}
+                    />
+
+                    <UserPost
+                      profilePic={user.photoURL}
+                      displayName={user.displayName}
+                      likeCount="12"
+                      commentCount="15"
+                      postImage="https://images.unsplash.com/photo-1507150823660-eed1895c23c7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                      user={user}
+                    />
+
+                    <UserPost
+                      profilePic={user.photoURL}
+                      displayName={user.displayName}
+                      likeCount="12"
+                      commentCount="15"
+                      postImage="https://images.unsplash.com/photo-1523221197642-4a2e4b6a3dcf?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                      user={user}
+                    />
+
+                    <UserPost
+                      profilePic={user.photoURL}
+                      displayName={user.displayName}
+                      likeCount="12"
+                      commentCount="15"
+                      postImage="https://images.unsplash.com/photo-1557948206-7478d769f813?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+                      user={user}
+                    />
+
+                    <UserPost
+                      profilePic={user.photoURL}
+                      displayName={user.displayName}
+                      likeCount="12"
+                      commentCount="15"
+                      postImage="https://images.unsplash.com/photo-1545249390-6bdfa286032f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=594&q=80"
+                      user={user}
+                    />
+                  </Row>
                 </Tab.Pane>
                 <Tab.Pane eventKey="second">This is status</Tab.Pane>
               </Tab.Content>
--- src/Components/UserPost/index.jsx
+++ src/Components/UserPost/index.jsx
@@ -1,26 +1,49 @@
 import { Box } from "@material-ui/core";
 import React from "react";
-import "./index.css"
+import "./index.css";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import { Col } from "react-bootstrap";
 
-const UserPost = ({profilePic, displayName, likeCount, commentCount,postImage,user}) => {
+const UserPost = ({
+  profilePic,
+  displayName,
+  likeCount,
+  commentCount,
+  postImage,
+  user,
+}) => {
   return (
     <>
-      <Box className="userPost">
-        <img src={postImage} alt="post imgae" />
-        <Box className="userPost__info">
-          <img src={profilePic} alt="user profile" />
+      <Col xs="12" md="4" lg="4">
+        <Box className="userPost">
+          <Box className="userPost__image" style={{backgroundImage: `url(${postImage})`}}>
+            
+          </Box>
 
-          <Box className="userPost__info__text">
-            <Box>
-
+          <Box className="userPost__info">
+            <Box className="userPost__info__text">
+              <img
+                src={profilePic}
+                alt="user profile"
+              />
+              <h6>{displayName}</h6>
             </Box>
 
-            <Box>
-              
+            <Box className="userPost__info__icon">
+              <Box className="userPost__info__icon--statistic">
+                <FavoriteBorderIcon />
+                <span>{likeCount}</span>
+              </Box>
+
+              <Box className="userPost__info__icon--statistic">
+                <ChatBubbleOutlineIcon />
+                <span>{commentCount}</span>
+              </Box>
             </Box>
           </Box>
         </Box>
-      </Box>
+      </Col>
     </>
   );
 };
